0ad728698a8f3ed67f709710408d7348a93df5f4
Khalil Mhelheli <kmhelheli@mobelite.fr>
2022-09-23 14:27:48 +0100

    create presentation layer

--- WatchMovie/WatchMovie/Presentation/Home/View/HomeViewController.swift
+++ WatchMovie/WatchMovie/Presentation/Home/View/HomeViewController.swift
@@ -6,3 +6,26 @@
 //
 
 import Foundation
+import UIKit
+
+class HomViewController: UIViewController{
+    
+    let viewModel: HomeViewControllerViewModel
+    
+    init(viewModel: HomeViewControllerViewModel) {
+        self.viewModel = viewModel
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .yellow
+        viewModel.getData()
+    }
+    
+    
+}
--- WatchMovie/WatchMovie/Presentation/Home/ViewModel/HomeViewControllerViewModel.swift
+++ WatchMovie/WatchMovie/Presentation/Home/ViewModel/HomeViewControllerViewModel.swift
@@ -6,3 +6,33 @@
 //
 
 import Foundation
+
+class HomeViewControllerViewModel {
+    
+    ///vars
+    private var manager: AppManagerProtocol
+
+    
+    private var mostPopularMovies: [MostPopularDataDetail] = []
+    private var mostPopularTvs: [MostPopularDataDetail] = []
+    
+    var reloadCollectionView: () -> Void = { }
+    
+    init(manager: AppManagerProtocol) {
+        self.manager = manager
+    }
+    
+    public func getData() {
+        manager.getMostPopularMovies { [weak self] result in
+            DispatchQueue.main.async {
+                switch result {
+                case .success(let response):
+                    self?.mostPopularMovies = response.items
+                    self?.reloadCollectionView()
+                case .failure(let error):
+                    print("Failed to fetch most popular movies: \(error)")
+                }
+            }
+        }
+    }
+}
