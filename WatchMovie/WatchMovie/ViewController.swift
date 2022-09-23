2290b33cc393bf34c6a009dfac7c049e04093e30
Khalil Mhelheli <kmhelheli@mobelite.fr>
2022-09-23 08:49:42 +0100

    first commit

---
+++ WatchMovie/WatchMovie/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WatchMovie
+//
+//  Created by Khalil Mhelheli on 23/9/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
