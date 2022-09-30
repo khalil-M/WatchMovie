66ae5391e15577001190b3077c2e8cbfae5b70af
Khalil Mhelheli <kmhelheli@mobelite.fr>
2022-09-30 15:11:16 +0100

    create layout compositional layout for collectionView

--- WatchMovie/WatchMovie/Presentation/Home/View/HomeViewController.swift
+++ WatchMovie/WatchMovie/Presentation/Home/View/HomeViewController.swift
@@ -17,6 +17,12 @@ class HomViewController: UIViewController{
         super.init(nibName: nil, bundle: nil)
     }
     
+    private var collectionView: UICollectionView = UICollectionView(frame: .zero, collectionViewLayout: UICollectionViewCompositionalLayout { sectionIndex, _ -> NSCollectionLayoutSection? in
+        return HomViewController.createSectionLayout(section: sectionIndex)
+    })
+    
+
+    
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
@@ -28,4 +34,94 @@ class HomViewController: UIViewController{
     }
     
     
+}
+
+extension HomViewController {
+    private static func createSectionLayout(section: sectionIndex) -> NSCollectionLayoutSection {
+        
+        let supplementaryViews = [
+            NSCollectionLayoutBoundarySupplementaryItem(
+                layoutSize: NSCollectionLayoutSize(
+                    widthDimension: .fractionalWidth(1),
+                    heightDimension: .absolute(50)
+                ),
+                elementKind: UICollectionView
+                    .elementKindSectionHeader,
+                alignment: .top
+            )
+        ]
+        
+        switch section {
+        case 0:
+            // Item
+            let item = NSCollectionLayoutItem(
+                layoutSize: NSCollectionLayoutSize(
+                    widthDimension:.fractionalWidth(0.3),
+                    heightDimension: .fractionalHeight(1.0)
+                )
+            )
+            item.contentInsets = NSDirectionalEdgeInsets(top: 2, leading: 2, bottom: 2, trailing: 2)
+            
+            let rowGroup = NSCollectionLayoutGroup.horizontal(
+                layoutSize: NSCollectionLayoutSize(
+                    widthDimension: .fractionalWidth(1.0),
+                    heightDimension: .fractionalHeight(1.0)
+                ),
+                subitem: item,
+                count: 3
+            )
+            
+            // Vertical Group
+            let verticalGroup = NSCollectionLayoutGroup.vertical(
+                layoutSize: NSCollectionLayoutSize(
+                    widthDimension: .fractionalWidth(1.0),
+                    heightDimension: .absolute(390)),
+                subitem: rowGroup,
+                count: 3
+            )
+            
+            let horizontalGroup = NSCollectionLayoutGroup.horizontal(
+                layoutSize: NSCollectionLayoutSize(
+                    widthDimension: .fractionalWidth(0.9),
+                    heightDimension: .absolute(390)
+                ),
+                subitem: verticalGroup,
+                count: 1
+            )
+            
+            // Section (made by groups)
+            let section = NSCollectionLayoutSection(group: horizontalGroup)
+            section.orthogonalScrollingBehavior = .groupPaging
+            section.boundarySupplementaryItems = supplementaryViews
+            return section
+        //case 1:
+        //    break
+        default:
+            // Item
+            let item = NSCollectionLayoutItem(
+                layoutSize: NSCollectionLayoutSize(
+                    widthDimension:.fractionalWidth(1.0),
+                    heightDimension: .fractionalHeight(1.0)
+                )
+            )
+            item.contentInsets = NSDirectionalEdgeInsets(top: 2, leading: 2, bottom: 2, trailing: 2)
+            
+            // Group (made by items)
+            
+            // Vertical Group
+            let group = NSCollectionLayoutGroup.vertical(
+                layoutSize: NSCollectionLayoutSize(
+                    widthDimension: .fractionalWidth(1.0),
+                    heightDimension: .absolute(390)),
+                subitem: item,
+                count: 1
+            )
+            
+            // Section (made by groups)
+            let section = NSCollectionLayoutSection(group: group)
+            section.orthogonalScrollingBehavior = .groupPaging
+            section.boundarySupplementaryItems = supplementaryViews
+            return section
+        }
+    }
 }
